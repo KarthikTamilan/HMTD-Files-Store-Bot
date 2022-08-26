2c488b6b2b30885cddaeecdace4bd8e306cf9dee
KarthikTamilan <karthikuk934@gmail.com>
2022-08-26 20:29:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -50,7 +50,7 @@ else:
     DISABLE_CHANNEL_BUTTON = False
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "<b>❌ Invalid Comment..! This Comment Only Access Owner or Admins So you Can Get File Links From : hmtd-movies.blogspot.com or @HMTD_Links</b>"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
